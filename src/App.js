02c1756b2e71595c5bd9844474aabbd9ea45ff50
JorPan <jbpanasewicz@gmail.com>
2021-03-01 13:19:56 -0700

    fetching characters

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import CharacterList from "./CharacterList";
 
 class App extends Component {
-
   state = {
-    characters: []
+    characters: [],
+  };
+
+  componentDidMount() {
+    fetch("https://rickandmortyapi.com/api/character/?page=2")
+      .then((response) => response.json())
+      .then(({ results }) =>
+        this.setState({
+          characters: results,
+        })
+      );
   }
-  
-  render(){
+
+  render() {
     return (
       <div className="App">
+        <CharacterList characters={this.state.characters} />
       </div>
     );
   }
